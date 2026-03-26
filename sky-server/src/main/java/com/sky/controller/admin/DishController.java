da5324b30ece63a0f467cf03dc9c33886650bf2a
starlight201 <1902469755@qq.com>
2026-03-26 20:10:20 +0800

    分类查询菜品功能代码实现

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,14 +1,14 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -29,5 +29,17 @@ public class DishController {
         String key = "dish_" + dishDTO.getCategoryId();
         //clearRedis(key);
         return Result.success();
+    }
+    /**
+     * 分页查询菜品
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page") // query
+    public Result<PageResult<DishVO>> page(DishPageQueryDTO dishPageQueryDTO) {
+        log.info("分页查询菜品：{}", dishPageQueryDTO);
+        PageResult<DishVO> querySet = dishService.queryPage(dishPageQueryDTO);
+        return Result.success(querySet);
     }
 }
