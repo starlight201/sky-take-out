ec2962a36e9e9e42d57c8695152c77193d3e68ed
starlight201 <1902469755@qq.com>
2026-04-03 20:04:06 +0800

    用户下单功能代码实现

---
+++ sky-server/src/main/java/com/sky/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.sky.controller;
+
+import com.sky.dto.OrdersPaymentDTO;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 用户下单
+     * 
+     * @param ordersSubmitDTO
+     * @return
+     */
+    @PostMapping("/submit")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO) {
+        log.info("用户下单，ordersSubmitDTO={}", ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+
+
+
+}
